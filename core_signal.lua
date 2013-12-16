0039f03660ee034573c3d3ead2fea160a3bcda44
reus <reusee@gmail.com>
2013-12-16 15:12:40 +0800

    core_signal: add gconnect()

--- core_signal.lua
+++ core_signal.lua
@@ -9,17 +9,26 @@ function core_signal_init(self)
 
   function self.connect_signal(name, func)
     if self._signals[name] == nil then
-      error('signal does not exists', name)
+      error('signal does not exists ' .. name)
     end
     table.insert(self._signals[name], func)
   end
 
   function self.emit_signal(name, ...)
     if self._signals[name] == nil then
-      error('signal does not exists', name)
+      error('signal does not exists ' .. name)
     end
     for _, func in pairs(self._signals[name]) do
       func(...)
     end
   end
+
+  -- safer
+  function self.gconnect(signal, func)
+    if func == nil then
+      error('cannot connect nil value')
+    end
+    signal:connect(func)
+  end
+
 end
