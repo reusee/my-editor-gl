36842dc3a62782d569be58bfff997a6088e2b414
reus <reusee@gmail.com>
2013-12-16 14:40:20 +0800

    add core_signal module

---
+++ core_signal.lua
@@ -0,0 +1,25 @@
+function core_signal_init(self)
+  self._signals = {}
+
+  function self.define_signal(name)
+    if self._signals[name] == nil then
+      self._signals[name] = {}
+    end
+  end
+
+  function self.connect_signal(name, func)
+    if self._signals[name] == nil then
+      error('signal does not exists', name)
+    end
+    table.insert(self._signals[name], func)
+  end
+
+  function self.emit_signal(name, ...)
+    if self._signals[name] == nil then
+      error('signal does not exists', name)
+    end
+    for _, func in pairs(self._signals[name]) do
+      func(...)
+    end
+  end
+end
