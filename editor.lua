f89ee2c7c371078282fd7d0631425e113ae808a0
reus <reusee@gmail.com>
2013-12-16 14:40:12 +0800

    refactor Editor class

--- editor.lua
+++ editor.lua
@@ -5,44 +5,51 @@ local GtkSource = lgi.require('GtkSource', '3.0')
 require 'core_defs'
 require 'core_buffer'
 require 'core_view'
+require 'core_signal'
 
-Editor = class{function(self)
-  self.widget = Gtk.Overlay()
+Editor = class{
+  function(self)
+    self.widget = Gtk.Overlay()
+  end,
 
-  -- init core modules
-  core_defs_init(self)
-  core_buffer_init(self)
-  core_view_init(self)
+  -- core modules
+  core_signal_init,
+  core_defs_init,
+  core_buffer_init,
+  core_view_init,
 
-  -- root grid
-  self.root_grid = Gtk.Grid()
-  self.widget:add(self.root_grid)
+  function(self)
+    -- root grid
+    self.root_grid = Gtk.Grid()
+    self.widget:add(self.root_grid)
 
-  -- views
-  self.views_grid = Gtk.Grid()
-  self.views_grid:set_row_homogeneous(true)
-  self.views_grid:set_column_homogeneous(true)
-  self.root_grid:attach(self.views_grid, 0, 0, 1, 1)
+    -- views
+    self.views_grid = Gtk.Grid()
+    self.views_grid:set_row_homogeneous(true)
+    self.views_grid:set_column_homogeneous(true)
+    self.root_grid:attach(self.views_grid, 0, 0, 1, 1)
 
-  -- areas
-  self.east_area = Gtk.Grid()
-  self.root_grid:attach(self.east_area, 1, 0, 1, 1)
-  self.west_area = Gtk.Grid()
-  self.root_grid:attach(self.west_area, -1, 0, 1, 1)
-  self.north_area = Gtk.Grid()
-  self.root_grid:attach(self.north_area, 0, -1, 2, 1)
-  self.south_area = Gtk.Grid()
-  self.root_grid:attach(self.south_area, 0, 1, 2, 1)
+    -- areas
+    self.east_area = Gtk.Grid()
+    self.root_grid:attach(self.east_area, 1, 0, 1, 1)
+    self.west_area = Gtk.Grid()
+    self.root_grid:attach(self.west_area, -1, 0, 1, 1)
+    self.north_area = Gtk.Grid()
+    self.root_grid:attach(self.north_area, 0, -1, 2, 1)
+    self.south_area = Gtk.Grid()
+    self.root_grid:attach(self.south_area, 0, 1, 2, 1)
 
-  -- font and style
-  self.style_scheme_manager = GtkSource.StyleSchemeManager.get_default()
-  self.style_scheme_manager:append_search_path(program_path())
-  self.style_scheme = self.style_scheme_manager:get_scheme('solarizeddark')
+    -- font and style
+    self.style_scheme_manager = GtkSource.StyleSchemeManager.get_default()
+    self.style_scheme_manager:append_search_path(program_path())
+    self.style_scheme = self.style_scheme_manager:get_scheme('solarizeddark')
 
-  -- extra modules
+    -- extra modules
 
-  -- first view
-  view = self.create_view()
-  self.views_grid:add(view.wrapper)
-end}
+    -- first view
+    view = self.create_view()
+    self.views_grid:add(view.wrapper)
+
+  end,
+}
 Editor.embed('widget')
