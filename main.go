d7219a8da7e14bc5bd23bb2fd8f5fd4aab99a46a
reus <reusee@gmail.com>
2013-12-18 13:17:39 +0800

    register several functions

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/user"
 	"path/filepath"
 	"runtime"
 	"time"
@@ -52,6 +53,23 @@ func main() {
 			}
 			return res
 		},
+		"pathsep": func() string {
+			return string(os.PathSeparator)
+		},
+		"homedir": func() string {
+			user, err := user.Current()
+			if err != nil {
+				return ""
+			}
+			return user.HomeDir
+		},
+		"fileexists": func(p string) bool {
+			_, err := os.Stat(p)
+			return err == nil
+		},
+		"mkdir": func(p string) bool {
+			return os.Mkdir(p, 0755) != nil
+		},
 
 		// time
 		"current_time_in_millisecond": func() int64 {
@@ -77,6 +95,46 @@ func main() {
 				return false
 			}
 			return info.IsDir()
+		},
+		"filemode": func(path string) os.FileMode {
+			info, err := os.Stat(path)
+			if err != nil {
+				return 0
+			}
+			return info.Mode()
+		},
+		"createwithmode": func(path string, mode uint32) bool {
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, os.FileMode(mode))
+			if err != nil {
+				return true
+			}
+			f.Close()
+			return false
+		},
+		"movefile": func(src, dst string) bool {
+			info, err := os.Stat(src)
+			if err != nil {
+				return true
+			}
+			mode := info.Mode()
+			_, err = os.Stat(dst)
+			if err == nil { // dst exists
+				return true
+			}
+			f, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE, mode)
+			if err != nil {
+				return true
+			}
+			defer f.Close()
+			content, err := ioutil.ReadFile(src)
+			if err != nil {
+				return true
+			}
+			f.Write(content)
+			return false
+		},
+		"rename": func(src, dst string) bool {
+			return os.Rename(src, dst) != nil
 		},
 	})
 
