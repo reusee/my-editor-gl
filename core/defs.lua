6b7b042ebf81737b128954365ce961a8226b96af
reus <reusee@gmail.com>
2013-12-23 13:48:47 +0800

    color scheme changes

--- core/defs.lua
+++ core/defs.lua
@@ -2,7 +2,7 @@ decl('core_defs_init')
 function core_defs_init(self)
   self.default_indent_width = 2
   self.default_font = Pango.FontDescription.from_string('Terminus 13')
-  self.default_scheme = 'molokai'
+  self.default_scheme = 'foo'
 
   self.BRACKETS = {
     ['('] = ')',
