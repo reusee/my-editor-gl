03a787ca5b13a726846cd0681c859ccc88d075c7
reus <reusee@gmail.com>
2014-01-01 14:48:42 +0800

    extra.lua: add one-line-function snippet

--- extra/lua.lua
+++ extra/lua.lua
@@ -28,6 +28,7 @@ function extra_lua_init(self)
 
     new_snippet('r/', 'lua_require', {"require '$1'$2"}, true)
     new_snippet('f/', 'lua_function', {'function$1($2)', '$>$3', '$<end$4'})
+    new_snippet('ff/', 'lua_oneline_function', {'function($1) $2 end$3'})
     new_snippet('if/', 'lua_if', {'if $1 then', '$>$2', '$<end$3'}, true)
     new_snippet('d/', 'lua_local', {'local $1'}, true)
     new_snippet('fo/', 'lua_for', {'for $1 = $2, $3 do', '$>$4', '$<end$5'}, true)
