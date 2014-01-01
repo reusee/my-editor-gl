3366ef36fe18316440cfd4831527ecd9d529cb84
reus <reusee@gmail.com>
2014-01-01 14:46:06 +0800

    extra.lua: some snippet should apply only at line start

--- extra/lua.lua
+++ extra/lua.lua
@@ -16,23 +16,27 @@ function extra_lua_init(self)
     end)
 
     -- snippets
-    local function new_snippet(trigger, name, snippet)
+    local function new_snippet(trigger, name, snippet, line_start)
       buffer.add_snippet(name, snippet)
+      local predict = function() return true end
+      if line_start then
+        predict = self.pattern_predict_cursor_at_line_start
+      end
       buffer.add_pattern(trigger, function() buffer.insert_snippet(name) end,
-        true, true, function() return true end)
+        true, true, predict)
     end
 
-    new_snippet('r/', 'lua_require', {"require '$1'$2"})
+    new_snippet('r/', 'lua_require', {"require '$1'$2"}, true)
     new_snippet('f/', 'lua_function', {'function$1($2)', '$>$3', '$<end$4'})
-    new_snippet('if/', 'lua_if', {'if $1 then', '$>$2', '$<end$3'})
-    new_snippet('d/', 'lua_local', {'local $1'})
-    new_snippet('fo/', 'lua_for', {'for $1 = $2, $3 do', '$>$4', '$<end$5'})
-    new_snippet('pa/', 'lua_pairs', {'for $1, $2 in pairs($3) do', '$>$4', '$<end$5'})
-    new_snippet('ip/', 'lua_ipairs', {'for $1, $2 in ipairs($3) do', '$>$4', '$<end$5'})
-    new_snippet('w/', 'lua_while', {'while $1 do', '$>$2', '$<end$3'})
-    new_snippet('s/', 'lua_self', {'self.$1'})
-    new_snippet('a/', 'lua_assign', {'$1 = $2'})
-    new_snippet('n/', 'lua_print', {'print($1)$2'})
+    new_snippet('if/', 'lua_if', {'if $1 then', '$>$2', '$<end$3'}, true)
+    new_snippet('d/', 'lua_local', {'local $1'}, true)
+    new_snippet('fo/', 'lua_for', {'for $1 = $2, $3 do', '$>$4', '$<end$5'}, true)
+    new_snippet('pa/', 'lua_pairs', {'for $1, $2 in pairs($3) do', '$>$4', '$<end$5'}, true)
+    new_snippet('ip/', 'lua_ipairs', {'for $1, $2 in ipairs($3) do', '$>$4', '$<end$5'}, true)
+    new_snippet('w/', 'lua_while', {'while $1 do', '$>$2', '$<end$3'}, true)
+    new_snippet('s/', 'lua_self', {'self.$1'}, true)
+    new_snippet('a/', 'lua_assign', {'$1 = $2'}, true)
+    new_snippet('n/', 'lua_print', {'print($1)$2'}, true)
 
   end)
 end
